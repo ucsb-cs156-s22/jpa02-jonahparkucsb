53d28de83dd38f842ff914a7b22dd2f4cbe154f6
Jonah Park <jonahpark@cs.ucsb.edu>
2022-04-10 15:49:31 -0700

    fixed return message

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from jonahpark!";
+        return "Greetings from jonahparkucsb!";
     }
     
 }
